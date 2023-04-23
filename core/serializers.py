74ee1b684dff10c1bcf28bfb282447b889f24182
Ruslan <zzzlyka221@gmail.com>
2023-04-23 23:11:56 +0500

    #2 - сериализаторы

---
+++ core/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import *
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class UniversitySerializer(serializers.ModelSerializer):
+    address = AddressSerializer
+
+    class Meta:
+        model = University
+        fields = '__all__'
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    university = UniversitySerializer
+
+    class Meta:
+        model = Department
+        fields = '__all__'
+
+
+class LecturerSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer
+
+    class Meta:
+        model = Lecturer
+        fields = '__all__'
